2f9a62c99eecf3c051f238899cc295c61a1e71aa
LissetteAcv2 <47862635+LissetteAcv2@users.noreply.github.com>
2026-01-15 22:51:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrar(id) {
+    const secciones = document.querySelectorAll('.seccion');
+    secciones.forEach(seccion => seccion.classList.remove('activa'));
+    document.getElementById(id).classList.add('activa');
+}
